dfb3759ee0ab269b296b059e234e89aabf0761f0
mh-anwar <mohammad.anwar2939@gmail.com>
2024-05-18 03:01:00 -0400

    Add /sendMoney endpoint

--- index.js
+++ index.js
@@ -4,19 +4,39 @@ import cors from 'cors';
 import path from 'path';
 import { nearConnection } from './near-functions.js';
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4000;
 
 // Parses HTTP Request body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/accountBalance');
 app.get('/', async (req, res) => {
-	const data = request.body;
+	const data = req.body;
 	const connection = await nearConnection();
 	const account = await connection.account(data.accountId);
 });
 
+app.post('/accountBalance', async (req, res) => {
+	const data = req.body;
+	const connection = await nearConnection();
+	const account = await connection.account(data.accountId);
+	return await account.getAccountBalance();
+});
+
+app.post('/sendMoney', async (req, res) => {
+	const data = req.body;
+	console.log(data);
+	console.log(data.accountId, data.receiverId, data.amount);
+	const connection = await nearConnection();
+	const account = await connection.account(data.accountId);
+	return await account.sendMoney(data.receiverId, data.amount);
+});
+
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
+});
+
+// ping endpoint
+app.get('/ping', (req, res) => {
+	res.send('pong');
 });
