9e8988e5a1a16183ecf3574aa86b7bb36d3eaa09
mh-anwar <mohammad.anwar2939@gmail.com>
2024-05-18 02:05:03 -0400

    Create API endpoints

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import path from 'path';
+import { nearConnection } from './near-functions.js';
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
 
 // Parses HTTP Request body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.get('/accountBalance');
+app.get('/', async (req, res) => {
+	const data = request.body;
+	const connection = await nearConnection();
+	const account = await connection.account(data.accountId);
 });
 
 app.listen(port, () => {
