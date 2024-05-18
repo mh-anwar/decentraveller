a6d1d274a2ab3dc87631a4bf4a1fa983e1c8831b
mh-anwar <mohammad.anwar2939@gmail.com>
2024-05-18 06:07:30 -0400

    Fix: Private key error, update private key usage

--- near-functions.js
+++ near-functions.js
@@ -1,28 +1,20 @@
 import { connect, keyStores, KeyPair } from 'near-api-js';
 
-const myKeyStore = new keyStores.InMemoryKeyStore();
-  
-const PRIVATE_KEY = "by8kdJoJHu7uUkKfoaLd2J2Dp1q1TigeWMG123pHdu9UREqPcshCM223kWadm";
+async function nearConnection(privKey) {
+	const myKeyStore = new keyStores.InMemoryKeyStore();
+	const keyPair = KeyPair.fromString(privKey);
+	await myKeyStore.setKey('testnet', 'habibrahman.testnet', keyPair);
 
-const keyPair = KeyPair.fromString(PRIVATE_KEY);
-
-await myKeyStore.setKey("testnet", "habibrahman.testnet", keyPair);
-
-
-const connectionConfig = {
-	networkId: 'testnet',
-    keyStore: myKeyStore,
-	nodeUrl: 'https://rpc.testnet.near.org',
-	walletUrl: 'https://testnet.mynearwallet.com/',
-	helperUrl: 'https://helper.testnet.near.org',
-	explorerUrl: 'https://testnet.nearblocks.io',
-};
-
-async function nearConnection() {
+	const connectionConfig = {
+		networkId: 'testnet',
+		keyStore: myKeyStore,
+		nodeUrl: 'https://rpc.testnet.near.org',
+		walletUrl: 'https://testnet.mynearwallet.com/',
+		helperUrl: 'https://helper.testnet.near.org',
+		explorerUrl: 'https://testnet.nearblocks.io',
+	};
 	const nearConnection = await connect(connectionConfig);
 	return nearConnection;
 }
-
-console.log(await nearConnection());
 
 export { nearConnection };
