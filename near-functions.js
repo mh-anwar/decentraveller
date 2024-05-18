9125a6829cd0faa85ad888020c2ddcb90f06bfb3
Abdul <lhlrahman@gmail.com>
2024-05-18 03:12:07 -0400

    Update NEAR connection configuration

--- near-functions.js
+++ near-functions.js
@@ -1,8 +1,17 @@
-import { connect } from 'near-api-js';
+import { connect, keyStores, KeyPair } from 'near-api-js';
+
+const myKeyStore = new keyStores.InMemoryKeyStore();
+  
+const PRIVATE_KEY = "by8kdJoJHu7uUkKfoaLd2J2Dp1q1TigeWMG123pHdu9UREqPcshCM223kWadm";
+
+const keyPair = KeyPair.fromString(PRIVATE_KEY);
+
+await myKeyStore.setKey("testnet", "habibrahman.testnet", keyPair);
+
 
 const connectionConfig = {
-	networkId: 'testnet' /* 
-	keyStore: myKeyStore, // first create a key store */, // skip signing transactions for now
+	networkId: 'testnet',
+    keyStore: myKeyStore,
 	nodeUrl: 'https://rpc.testnet.near.org',
 	walletUrl: 'https://testnet.mynearwallet.com/',
 	helperUrl: 'https://helper.testnet.near.org',
@@ -13,5 +22,7 @@ async function nearConnection() {
 	const nearConnection = await connect(connectionConfig);
 	return nearConnection;
 }
+
+console.log(await nearConnection());
 
 export { nearConnection };
