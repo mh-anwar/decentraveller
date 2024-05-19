2deab689e694989d1e1a998e731873e080f08c1d
mh-anwar <mohammad.anwar2939@gmail.com>
2024-05-19 04:34:27 -0400

    Add logging

--- index.js
+++ index.js
@@ -106,6 +106,7 @@ app.post('/gptCompletion', async (req, res) => {
 
 	const messages = extractMessages(inputJson);
 	const response = await gptCompletion(messages);
+	console.log('Our response is: ' + response);
 	res.send(response);
 });
 
